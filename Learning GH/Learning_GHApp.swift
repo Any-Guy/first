cd47255d78dc3cb889fffe5970019d0e837cdd85
Swapnil Pal <iswap0@gmail.com>
2023-08-13 20:47:13 +0530

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by Apple on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
