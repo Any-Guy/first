234d32b07c642df9622aa561ac1fdf39631252f4
Any-Guy <142162912+Any-Guy@users.noreply.github.com>
2023-08-13 21:29:30 +0530

    Update ContentView.swift to what's up

--- Learning GH/ContentView.swift
+++ Learning GH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Swapnil!")
+            Text("What's up, Swapnil!")
         }
         .padding()
     }
